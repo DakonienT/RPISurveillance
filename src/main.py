c3c83254833adb6a554f882b20027463bae1b03c
DakonienT <38476962+DakonienT@users.noreply.github.com>
2023-03-21 23:21:35 +0100

    Implementation of motion detector

--- src/main.py
+++ src/main.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template, Response, request
 import logging
 from xmlrpc.client import boolean
 import numpy as np
-#import imutils
+import imutils
 from datetime import datetime
 import socket
-
+from flask import request
+#from flask import jsonify
 
 
 app = Flask(__name__, template_folder=r'C:\Users\simon\Desktop\RPISurveillance\templates')
@@ -41,11 +42,16 @@ class SingleMotionDetector:
             return
         cv2.accumulateWeighted(image, self.bg, self.accumWeight)
     def detect(self, image, tVal=25):
+        
         delta = cv2.absdiff(self.bg.astype("uint8"), image)
         thresh = cv2.threshold(delta, tVal, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.erode(thresh, None, iterations=2)
         thresh = cv2.dilate(thresh, None, iterations=2)
+        thresh = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
         contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
+        (minX, minY) = (np.inf, np.inf)
+        (maxX, maxY) = (-np.inf, -np.inf)
         if len(contours) == 0:
             #No motion detected
             return None
@@ -57,9 +63,17 @@ class SingleMotionDetector:
             return (thresh, (minX, minY, maxX, maxY))
     
 recorder_object = Recorder()
+
+motion_detector = SingleMotionDetector(accumWeight=.1)
+
+
 def genFrames():
+    total_frames = 0
+    minimum_frames_for_bg = 32
     while True:
         success, frame = camera.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(frame, (7, 7), 0)
         #print(frame[0])
         #width = int(frame[1] * scale_percent / 100)
         #height = int(frame[0] * scale_percent / 100)
@@ -73,9 +87,23 @@ def genFrames():
         recorder_object.image = frame
         if(recorder_object.record):
             recorder_object.writer.write(frame)
+        
+        #Perform motion detection
+        #frame = imutils.resize(frame, width=400)
+
+        if total_frames > minimum_frames_for_bg:
+            motion = motion_detector.detect(gray)
+            if motion is not None:
+                (thresh, (minX, minY, maxX, maxY)) = motion
+                cv2.rectangle(frame, (minX, minY), (maxX, maxY), (0, 0, 255), 2)
+                logging.info("Motion detected !")
+        motion_detector.update(gray)
+        total_frames += 1
+        
+        
         if not success:
-            break
             logging.error("Error : cannot get image")
+            break
         else:
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
@@ -84,7 +112,7 @@ def genFrames():
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    
+    logging.info("Client connected with ip " + str(request.remote_addr))
     if request.method == 'POST':
         if request.form.get('Start recording') == 'Start recording':
 
